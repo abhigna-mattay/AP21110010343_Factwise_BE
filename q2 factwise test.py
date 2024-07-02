2d867bc2d66dd69b68ccd26e725cf00ccf7461ac
Lakshmi Abhigna Mattay <112966093+abhigna-mattay@users.noreply.github.com>
2024-07-02 16:02:57 +0530

    Add files via upload

---
+++ q2 factwise test.py
@@ -0,0 +1,38 @@
+# maximum score by picking cards
+
+cardPoints=list(map(int,input().split()))
+k=int(input())
+score=0
+if len(cardPoints)==k:
+    score=sum(cardPoints)
+else:
+    for turn in range(k):
+        n=len(cardPoints)
+        if n==0:
+            break
+        fnum=cardPoints[0]
+        lnum=cardPoints[n-1]
+        
+        if fnum==lnum:
+            # if first and last are equal then we choose based on the next of first and previous of last
+            if cardPoints[n-2]>cardPoints[1]:
+                score+=lnum
+                cardPoints.pop()
+            elif cardPoints[n-2]<cardPoints[1]:
+                score+=fnum
+                cardPoints.pop(0)
+            else:
+                score+=lnum
+                cardPoints.pop()
+        else:
+            # if first and last are not equal we decide based on maximum of first and last
+            if fnum>lnum:
+                score+=fnum
+                cardPoints.pop(1)
+            elif fnum<lnum:
+                score+=lnum
+                cardPoints.pop()
+                
+print("Maximum score possible is : ",score)
+                
+                
